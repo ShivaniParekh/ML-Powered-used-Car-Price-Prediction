d4ff3f34c285d0a060ee4c46d1535b50feb3c655
ShivaniParekh <shivaniparekh2197@gmail.com>
2024-04-29 21:31:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,7 @@ import jsonify
 import requests
 import pickle
 import numpy as np
-import sklearn
 from sklearn.preprocessing import StandardScaler
-
-
 
 app = Flask(__name__)
 model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
